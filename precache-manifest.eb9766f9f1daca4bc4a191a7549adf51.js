96e3c3896c6831d9d968a9db4ef92d5fd7e958e4
nomen1 <olesiamakarenko5115@gmail.com>
2020-06-02 12:11:35 +0300

    Updates

---
+++ precache-manifest.eb9766f9f1daca4bc4a191a7549adf51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45179f06e039794dad3a4211ea6eb834",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "8ef78b04e167a1512e9d",
+    "url": "/test-task/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "74bedc4c317ef7665364",
+    "url": "/test-task/static/js/2.1f77cf69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-task/static/js/2.1f77cf69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef78b04e167a1512e9d",
+    "url": "/test-task/static/js/main.bd59ce2f.chunk.js"
+  },
+  {
+    "revision": "df15afeac7e4922fd30b",
+    "url": "/test-task/static/js/runtime-main.fd73edcb.js"
+  }
+]);
